c0575c036cda2a86cefadd54cea589a130d5f9aa
ms-ad\Marcos Longo <marcoslongo@pecege.com>
2024-04-03 15:07:36 -0300

    redeploy

--- src/api/apollo-client.ts
+++ src/api/apollo-client.ts
@@ -5,7 +5,7 @@ export const GqlClient = new ApolloClient({
     cache: new InMemoryCache(),
     defaultOptions: {
         watchQuery: {
-            nextFetchPolicy: "network-only",
+            nextFetchPolicy: "cache-only",
         }
     }
 });
