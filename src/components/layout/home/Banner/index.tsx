8cf80d4f553972a03f7075593b51b50843ae22f4
ms-ad\Marcos Longo <marcoslongo@pecege.com>
2024-04-03 16:06:21 -0300

    refactor: ajusta botão do banner de contato

--- src/components/layout/home/Banner/index.tsx
+++ src/components/layout/home/Banner/index.tsx
@@ -9,10 +9,10 @@ export async function Banner() {
     return (
         <section className="container max-w-[1000px] mx-auto pt-36 px-4 md:px-0">
             <div className="flex flex-wrap items-center justify-between">
-                <article className="w-full text-center justify-center md:text-left md:w-2/4 flex flex-col gap-5">
+                <article className="w-full text-center items-center md:items-start md:text-left md:w-2/4 flex flex-col gap-5">
                     <h2 className="text-5xl font-bold">{banner.titulo}</h2>
                     <p className="text-lg">{banner.texto}</p>
-                    <Link className="w-48 text-center rounded-3xl border border-solid border-white px-4 py-2 hover:bg-[#A79A86] hover:border-[#A79A86] transition" href="#contato">Entre em contato</Link>
+                    <Link className="w-48 mb-4 md:mb-0 text-center rounded-3xl border border-solid border-white px-4 py-2 hover:bg-[#A79A86] hover:border-[#A79A86] transition" href="#contato">Entre em contato</Link>
                 </article>
                 <figure className="w-full md:w-2/4">
                     <Image
